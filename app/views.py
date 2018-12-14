c8e52d28ca98a7a379185d7c18a29e78579f3d91
basecamp <jmorgan@basecampcodingacademy.org>
2018-12-14 09:44:26 -0600

    Author: John Morgan
    Udated GetHome so the it contains the blog posts

--- app/views.py
+++ app/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.views import View
+from . import models
 
 # Create your views here.
 
 
 class HomePage(View):
     def get(self, request):
-        return render(request, "home.html")
+        return render(request, "home.html",
+                      {'blog_posts': models.BlogPost.get_blogs()})
