80c77103ed12c15b987d990207ced9c5158d548b
basecamp <jmorgan@basecampcodingacademy.org>
2018-12-14 09:43:20 -0600

    Author: GInger Keys
    Created test_home_get_blogs to test that Get Home gets blogs

--- app/test/test_views.py
+++ app/test/test_views.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from django.urls import reverse
+from unittest.mock import patch
 
 # Create your tests here.
 
@@ -9,3 +10,10 @@ class TestHomeView(TestCase):
     def test_home(self):
         response = self.client.get(reverse('home'))
         self.assertTemplateUsed(response, 'home.html')
+
+    @patch('app.models.BlogPost.get_blogs')
+    def test_home_gets_blogs(self, get_blogs):
+        response = self.client.get(reverse('home'))
+
+        self.assertEqual(response.context['blog_posts'],
+                         get_blogs.return_value)
